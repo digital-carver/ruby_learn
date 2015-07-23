2ee48ea4d3667efeabcab33b6ee11e95bff81cd0
SundaraRaman R <sundar@abiteasier.in>
2015-07-23 13:03:24 +0530

    Rearrange stuff in silly shoes app

--- shoes_learn/ennadhu.rb
+++ shoes_learn/ennadhu.rb
@@ -1,7 +1,11 @@
 Shoes.app do
-    para "Ennadhu, Gandhi setthuttaaraa??"
-    @yes_button = button "Yes"
-    @response_p = para ""
+    stack do
+        flow do
+            para "Ennadhu, Gandhi setthuttaaraa??"
+            @yes_button = button "Yes"
+        end
+        @response_p = para ""
+    end
     @yes_button.click do
         @response_p.replace "அய்யகோ! நான் என் செய்வேன்!!"
     end
