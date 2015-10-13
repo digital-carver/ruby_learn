38f9c0df07d56e081cbc94a2dde7765d4fc8a1e9
SundaraRaman R <sundar@abiteasier.in>
2015-10-13 15:59:21 +0530

    Complicated but (hopefully) reliable way of age calculation

--- age_in_days.rb
+++ age_in_days.rb
@@ -1,5 +1,15 @@
+require 'date'
 
 def age_in_days(day, month, year)
+    born_day = Date.new(year, month, day)
+    today = Date.today
+    birthdays = born_day.step(today).select{|d|
+        d.month == month && d.day == day
+    }
+    num_birthdays = birthdays.size
+    # Count out the day you were born, as that doesn't count as age 1
+    age = num_birthdays - 1
+    print age
 end
 
 puts age_in_days(15, 8, 1947)
